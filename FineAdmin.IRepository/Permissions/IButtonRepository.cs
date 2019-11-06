61e057d1fb37bb506ee6bc4c2843f08effb2ef62
Liu_Cabbage <178899573@qq.com>
2019-11-06 17:40:56 +0800

    Fine

---
+++ FineAdmin.IRepository/Permissions/IButtonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FineAdmin.Model;
+
+namespace FineAdmin.IRepository
+{
+    public interface IButtonRepository : IBaseRepository<ButtonModel>
+    {
+
+    }
+}
---
+++ FineAdmin.Repository/Permissions/ButtonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FineAdmin.IRepository;
+using FineAdmin.Model;
+
+namespace FineAdmin.Repository
+{
+    public class ButtonRepository : BaseRepository<ButtonModel>, IButtonRepository
+    {
+
+    }
+}
