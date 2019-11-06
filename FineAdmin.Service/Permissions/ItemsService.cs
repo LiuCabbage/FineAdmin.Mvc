cdf57e92a7dbcfd43da4f92dfe490d51baf984db
Liu_Cabbage <178899573@qq.com>
2019-11-06 18:02:58 +0800

    Fine

---
+++ FineAdmin.Service/Permissions/ItemsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FineAdmin.IService;
+using FineAdmin.Model;
+
+namespace FineAdmin.Service
+{
+    public class ItemsService : BaseService<ItemsModel>, IItemsService
+    {
+
+    }
+}
