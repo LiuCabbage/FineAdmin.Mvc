1813c60080e9712918b2700dcc90be1e05d9168a
大番薯没有心 <178899573@qq.com>
2022-06-12 07:31:27 +0000

    update FineAdmin.Web/Controllers/HomeController.cs. 防止伪造文件上传

--- FineAdmin.Web/Controllers/HomeController.cs
+++ FineAdmin.Web/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -43,6 +43,15 @@ namespace FineAdmin.Web.Controllers
                     uploadFile.msg = "上传出错!请检查文件名或文件内容";
                     return Json(uploadFile, JsonRequestBehavior.AllowGet);
                 }
+
+                if (!IsAllowedExtension(file))
+                {
+                    _uploadFile.code = -1;
+                    _uploadFile.data = new { src = "" };
+                    _uploadFile.msg = "检测到上传文件有问题!";
+                    return Json(_uploadFile);
+                }
+                
                 //定义本地路径位置
                 string localPath = Server.MapPath("~/Upload");
                 string filePathName = string.Empty; //最终文件名
@@ -67,5 +76,67 @@ namespace FineAdmin.Web.Controllers
             }
         }
 
+        /// <summary>
+        /// 判断文件是否正常，防止伪造文件上传
+        /// </summary>
+        /// <param name="hifile"></param>
+        /// <returns></returns>
+        private static bool IsAllowedExtension(HttpPostedFileBase postedFile)
+        {
+            int fileLen = postedFile.ContentLength;
+            byte[] imgArray = new byte[fileLen];
+            postedFile.InputStream.Read(imgArray, 0, fileLen);
+            System.IO.MemoryStream fs = new System.IO.MemoryStream(imgArray);
+            System.IO.BinaryReader r = new System.IO.BinaryReader(fs);
+
+            string fileclass = "";
+            byte buffer;
+            try
+            {
+                buffer = r.ReadByte();
+                fileclass = buffer.ToString();
+                buffer = r.ReadByte();
+                fileclass += buffer.ToString();
+            }
+            catch
+            {
+                return false;
+            }
+            r.Close();
+            fs.Close();
+
+            /*文件扩展名说明 
+            jpg：255216 
+            bmp：6677 
+            png：13780
+            gif：7173 
+            xls,doc,ppt：208207 
+            rar：8297 
+            zip：8075 
+            txt：98109 
+            pdf：3780 
+            */
+            bool ret = false;
+            String[] fileType = {
+            "255216",
+            "6677",
+            "13780",
+            "7173",
+            "208207",
+            "8297",
+            "8075",
+            "98109",
+            "3780"
+            };
+            for (int i = 0; i < fileType.Length; i++)
+            {
+                if (fileclass == fileType[i])
+                {
+                    ret = true;
+                    break;
+                }
+            }
+            return ret;
+        }
     }
 }
